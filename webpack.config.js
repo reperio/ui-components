74085f07005d4ea41917ddf3ae1f0730ed4974b8
Brian Adams <brian@brianadams.codes>
2018-08-14 13:37:50 -0400

    REP-138 Get the components package built correctly.

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src/index.ts'),
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'lib'),
         library: '',
         libraryTarget: 'commonjs'
     },
