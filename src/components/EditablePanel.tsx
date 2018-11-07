27dd544aeb6ed838e57c67f3fafefc29d59fe632
= <reser13@gmail.com>
2018-11-07 14:16:51 -0500

    REP-255 - Added a new prop called permissionToEdit to editable panel

--- src/components/EditablePanel.tsx
+++ src/components/EditablePanel.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
 interface PanelProps {
-    active: boolean;
+    cancel(): void;
     onClick(): void;
     submit(): void;
-    cancel(): void;
+    active: boolean;
     children: JSX.Element;
+    permissionToEdit?: boolean;
 }
 
 const EditablePanelOverlay = () => <div className="r-editable-panel-overlay"></div>
 
 const EditablePanel: React.SFC<PanelProps> = props => (
-    <div className={`${props.active ? 'r-wrapper-editable-panel-open' : 'r-wrapper-editable-panel'} row`} 
+    <fieldset disabled={!props.active} className={`${props.active ? 'r-wrapper-editable-panel-open' : props.permissionToEdit ? 'r-wrapper-editable-panel' : 'r-wrapper-editable-panel-disabled'} row`} 
         style={props.active ? {zIndex: 10} : {}}
         onClick={props.onClick}>
             {props.active ?                                     
@@ -19,12 +20,14 @@ const EditablePanel: React.SFC<PanelProps> = props => (
                     <i className="r-editable-panel-control-item fa fa-ban fa-lg" onClick={() => props.cancel()}></i>
                     <i className="r-editable-panel-control-item fa fa-check fa-lg" onClick={() => props.submit()}></i>
                 </div> 
-            : 
+            : null }
+            {!props.active && props.permissionToEdit ? 
                 <div className="r-editable-panel-controls">
                     <i className="r-editable-panel-control-item fa fa-pencil fa-lg"></i>
-                </div> }
+                </div>
+            : null}
             {props.children}
-    </div>
+    </fieldset>
 )
 
 export { EditablePanel, EditablePanelOverlay };
