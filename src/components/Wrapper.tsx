509da671f96d270e074c1213b8de443e963050ef
Will Osbourne <will@osbourne.org>
2019-07-12 16:26:19 -0400

    id:3458 adding maxWidth to Wrapper component

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -2,15 +2,35 @@ import * as React from 'react';
 
 interface WrapperProps {
     children: React.ReactNode 
-    flexColumnDirection?: boolean
+    flexColumnDirection?: boolean,
+    maxWidth?: number
 }
 
 const Wrapper:React.SFC<WrapperProps> = props => {
-    return <div className={props.flexColumnDirection ? 'r-wrapper r-wrapper-column-direction' : 'r-wrapper'}>{props.children}</div>;
+    const maxWidth = props.maxWidth === 0 ? 'none' : props.maxWidth;
+    const parentDivStyle: React.CSSProperties = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+
+    const childDivStyle: React.CSSProperties = {
+        maxWidth,
+        alignSelf: 'center'
+    }
+
+    return (
+        <div style={parentDivStyle}>
+            <div className={props.flexColumnDirection ? 'r-wrapper r-wrapper-column-direction' : 'r-wrapper'} style={childDivStyle}>
+                {props.children}
+            </div>
+        </div>
+    );
 }
 
 Wrapper.defaultProps = {
-    flexColumnDirection: false
+    flexColumnDirection: false,
+    maxWidth: 1200
 }
 
 export { Wrapper };
